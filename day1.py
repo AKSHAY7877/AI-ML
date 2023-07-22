06aff9aaab3374910df9826532d7e8b25bd31aa4
AKSHAY7877 <2020pietcsakshay19@poornima.org>
2023-07-22 10:15:51 +0530

    git setup

--- day1.py
+++ day1.py
@@ -26,16 +26,20 @@
 
 # Create Repository - Done
 # Initialize Repo - Done
-    # this can be done by clicking source control and click initialize repo
+    # this can be done by clicking source control tab and click initialize repo
     #      or 
     # by using command prompt   --->    git init
 
 # user configuration 
-        git config --global user.name "AKSHAY7877"
-        git config --global user.email "akshaysep12@gmail.com"
+    #    git config --global user.name "AKSHAY7877"
+    #    git config --global user.email "2020pietcsakshay19@poornima.org"
+
+# Remote Repository Connect
+    # git remote add origin "https://github.com/AKSHAY7877/AI-ML-day1.git"
 
 # Daily process
     # save & commit all changes
     # command is         ----->      git add .        use to make file stage
     #                    ----->      git commit -m "First time commit"
-
+  # push the changes to github
+    #                    ----->  git push origin main                      # git is software    push is process      origin  refer to things present at server side        main is the name of branch
