d9efb016011d60ddd4759b2654f08c193fd19c76
AKSHAY7877 <2020pietcsakshay19@poornima.org>
2023-07-22 09:59:12 +0530

    First time commit

---
+++ day1.py
@@ -0,0 +1,41 @@
+// python fundamentals
+
+# Brackets
+# Identifiers
+# operators
+# Data & types
+# Built-in Functions for Data types
+# control Statements
+## conditional
+## Looping
+## Comprehensions
+# user defined functions
+## function Parameterization
+## Lambda Expressins
+## Generators
+## Decorators
+# Object Oriented Programming
+# Modules & Packages in python
+
+
+# GUI Development using Python
+
+
+## GIT setup
+    # website   ---->    git-csm.com
+
+# Create Repository - Done
+# Initialize Repo - Done
+    # this can be done by clicking source control and click initialize repo
+    #      or 
+    # by using command prompt   --->    git init
+
+# user configuration 
+        git config --global user.name "AKSHAY7877"
+        git config --global user.email "akshaysep12@gmail.com"
+
+# Daily process
+    # save & commit all changes
+    # command is         ----->      git add .        use to make file stage
+    #                    ----->      git commit -m "First time commit"
+
