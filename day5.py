6075a41c583e9163932d22e9d02805599b36b985
AKSHAY7877 <2020pietcsakshay19@poornima.org>
2023-07-30 10:13:42 +0530

    after practive

--- day5.py
+++ day5.py
@@ -1,32 +1,31 @@
-import day5_pack.test as test
-test.hi()
+# import day5_pack.test as test
+# test.hi()
 
-import day5_pack
-day5_pack.test.hi()
-day5_pack.xyz()
+# import day5_pack
+# day5_pack.test.hi()
+# day5_pack.xyz()
 
-from day5_pack import test
-test.hi()
+# from day5_pack import test
+# test.hi()
 
-from day5_pack import xyz
-xyz()
-
-
+# from day5_pack import xyz
+# xyz()
 
 
-# #import day5_lib
-# # from day5_lib import  multiplier
-# #import day5_lib as d5                # alias
-# from day5_lib import multiplier as mt,data
+values = input('Enter the values seperated by comma (,):')
+values = [eval(i) for i in values.split(',')]
 
-# values = input('Enter values seperated by comma(,): ')
-# #print(values.split(','))
-# values = [eval(val) for val in values.split(',')]
-# print(values)
-# #print(day5_lib.multiplier(*values))        # we are breaking it down or usko open krke de rhe hai alg alg kere 
-# #print(multiplier(*values)) 
-# #print(d5.multiplier(*values))
-# print(mt(*values))
-# print(data * 3)
+import day5_lib
+print(day5_lib.multiplier(*values))      # we are breaking it down or usko open krke de rhe hai alg alg krke
+
+from day5_lib import multiplier
+print(multiplier(*values))
+
+import day5_lib as d5
+print(d5.multiplier(*values))
+
+from day5_lib import multiplier as mt, data
+print(multiplier(*values))
+print(data*3)
 
 
--- day5_lib.py
+++ day5_lib.py
@@ -9,7 +9,7 @@ def multiplier(*a):
         k *= i
     return k
 
-print(__name__)
+print(__name__)           # when this file is run __name__ returns __name__ and when this file is imported in another file __name__ returns the file name
 
 if __name__ == '__main__':
     print(data)
